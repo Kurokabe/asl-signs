82ede066cd6dd1574742bf684642b1bbef972509
farid.abdalla <farid.abdalla@he-arc.ch>
2023-03-26 10:57:49 +0200

    Add features (distance between fingers)

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.loggers import TensorBoardLogger
 
 from src.data.datamodule import SignDataModule
 from src.model.baseline import SignModel
@@ -10,11 +11,12 @@ from src.model.baseline import SignModel
 
 def cli_main():
     torch.set_float32_matmul_precision("medium")
+    logger = TensorBoardLogger("lightning_logs", name="baseline")
     checkpoint_callback = ModelCheckpoint(save_top_k=1, monitor="val/loss")
     cli = LightningCLI(
         SignModel,
         SignDataModule,
-        trainer_defaults={"callbacks": [checkpoint_callback]},
+        trainer_defaults={"callbacks": [checkpoint_callback], "logger": logger},
     )
 
     # summary(cli.model, input_size=(2, 3, 128, 256))
